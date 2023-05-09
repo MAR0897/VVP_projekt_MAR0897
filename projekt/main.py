4c84efb8f5dd5021746ad575b014957f834a845a
Oskar <oskar.martinak.st@vsb.cz>
2023-05-09 12:20:16 +0200

    trida

--- projekt/main.py
+++ projekt/main.py
@@ -1,53 +1,43 @@
 import json
 import importlib
-import argparse
-import planet
-importlib.reload(planet)
-from planet import Planet
-import plotting as plot
-importlib.reload(plot)
+from .planet import Planet
+from .plotting import plot
 from numpy.linalg import norm
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_file", type=str, required=True, help="path to JSON file with initial data")
-parser.add_argument("--n", type=int, default=100, help="number of iterations")
-parser.add_argument("--timestep", type=float, default=86400, help="time value in seconds (time passed after each iteration)")
-parser.add_argument("--print_mode", type=int, default=1, help="printing mode: 0 - line plot, 1 - system plot (one body in the center), 2 - scatter plot, 3 - animation")
-parser.add_argument("--random", type=int, default=0, help="randomized initial planet parameters")
-parser.add_argument("--container", type=int, default=0, help="checks if the planets became degenerated")
-args = parser.parse_args()
+class Simulace:
+    def __init__(self, iter_n: int = 100, timestep: float = 86400, random: int = 0, container: int = 0) -> None:
+        #Setting initial args
+        self.iter_n = iter_n
+        self.dt = timestep
+        self.container = container
+        self.random = random
+        self.planets = []
 
-#Setting initial args
-iter_n: int = args.n
-print_mode: int = args.print_mode
-dt: float = args.timestep
-container: int = args.container
-random = args.random
-planets: list = []
+    def read_from_file(self, input_file: str) -> None:
+        #Reading from file, class initialization
+        with open(input_file, "r") as file:
+            init_data: dict = json.load(file)
+        for name, data in init_data.items():
+            if self.random == 0:
+                planet = Planet(name, data["position"], data["velocity"], data["mass"], self.iter_n)
+            else:
+                planet = Planet.random(name, norm(data["position"]), norm(data["velocity"]), data["mass"], self.iter_n)
+            self.planets.append(planet)
 
-#Reading from file, class initialization
-with open(args.input_file, "r") as file:
-    init_data: dict = json.load(file)
-for name, data in init_data.items():
-    if random == 0:
-        planet = Planet(name, data["position"], data["velocity"], data["mass"], iter_n)
-    else:
-        planet = Planet.random(name, norm(data["position"]), norm(data["velocity"]), data["mass"], iter_n)
-    planets.append(planet)
+    def simulate(self) -> None:
+        #Moving planets
+        degenerated = False
+        for i in range(self.iter_n):
+            Planet.update_coords(Planet, self.planets, i, self.dt)
+            if self.container==1:
+                for planet in self.planets:
+                    if Planet.is_too_far(planet):
+                        print("Simulation probably degenerated")
+                        degenerated = True
+                        break
+                if degenerated:
+                    self.iter_n = i
+                    break
 
-#Moving planets
-degenerated = False
-for i in range(iter_n):
-    Planet.update_coords(Planet, planets, i, dt)
-    if container==1 and i%100==0:
-        for planet in planets:
-            if Planet.is_too_far(planet):
-                print("Simulation probably degenerated")
-                degenerated = True
-                break
-        if degenerated:
-            iter_n = i
-            break
-
-#Printing results
-plot.plot(planets, print_mode, iter_n)
+    def print(self, print_mode: int = 0) -> None:
+        plot(self.planets, print_mode, self.iter_n)
